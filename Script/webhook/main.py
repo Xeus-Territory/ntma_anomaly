54bb25b82a3966464c7530f51f6954885450e268
XeusNguyen <nghia.nguyenduc@orientsoftware.com>
2023-04-20 18:09:18 +0700

    Threading: Update threading for scaling :coffee:

--- Script/webhook/main.py
+++ Script/webhook/main.py
@@ -2,6 +2,9 @@ from flask import Flask, request
 from dotenv import load_dotenv
 import os
 import telebot
+import threading
+import requests
+import time
 
 # Get environment variable 
 load_dotenv()
@@ -11,6 +14,13 @@ CHAT_ID   = os.getenv('CHAT_ID')
 bot = telebot.TeleBot(BOT_TOKEN)
 
 app = Flask(__name__)
+
+# Basic parameters of prometheus and infra settings
+replica_name = []
+monitoring_alert_prometheus = []
+current_replica = 1
+thread_list = []
+scale_down_flag = False
 
 def send_test_message(data):
     """Generates a Message object and sends it to the telegram by bot
@@ -36,34 +46,92 @@ def send_test_message(data):
         bot.send_message(CHAT_ID, message)
     except Exception as ex:
         print(ex)
-
-def scaling_job(name_service, action):
+        
+def check_health_alert(deplay_check_time=30):
+    global monitoring_alert_prometheus
+    while True:
+        response = requests.get('http://localhost:9090/api/v1/alerts')
+        monitoring_alert = response.json()['data']['alerts']
+        if len(monitoring_alert) == 0:
+            monitoring_alert_prometheus = []
+            time.sleep(deplay_check_time)
+        if len(monitoring_alert) > 0:
+            for index in range(0, len(monitoring_alert)):
+                if monitoring_alert[index]['labels']['name'] not in monitoring_alert_prometheus:
+                    monitoring_alert_prometheus.clear()
+                    monitoring_alert_prometheus.append(monitoring_alert[index]['labels']['name'])
+            time.sleep(deplay_check_time)
+            
+            
+        
+def scaling_replica(name_container, max_scaling_service=1):
     """Scaling job for a given
 
     Args:
         name_service (string): which container of service meet trouble and need to scale
-        action (string): string with scaling (UP or DOWN)
+        max_scaling_service (int): maximum scaling service can be reached for this container. Default = 1
 
-    """    
-    print("Scaling " + action + "...")
-    os.system('./scale-service-swarm.sh ' + name_service + ' ' + action)
+    """
+    global monitoring_alert_prometheus, replica_name, current_replica, scale_down_flag
+    print("Scaling up for dedicated " + name_container + " .....")
+    os.system('./scale-service-swarm.sh ' + name_container + ' ' + "up " + str(max_scaling_service))
+    print("Scaling successful for " + name_container)
+    current_replica += max_scaling_service
+    while True:
+        if name_container in monitoring_alert_prometheus:
+            continue
+        if name_container not in monitoring_alert_prometheus:
+            if scale_down_flag == False:
+                scale_down_flag = True
+                os.system('./scale-service-swarm.sh ' + name_container + ' ' + "down " + str(max_scaling_service))
+                current_replica -= max_scaling_service
+                replica_name.remove(name_container)
+                print("The current replica is scaling down, application have stable state !!!!")
+                scale_down_flag = False
+                break
+            if scale_down_flag == True:
+                continue
+
     
-@app.route('/alerts', methods=['POST'])
-def alerts():
-    """Receive alerts from prometheus and trigger scale and notify for telegram
-
-    Returns:
-        string: "OK" for comfirm everything is work
-    """    
-    data = request.json
-    data = data["alerts"][0]
+def scaling_node(name_node, max_scaling_node=1):
+    pass
     
-    # Your analysis and processing here!
-    scaling_job(request.json['alerts'][0]['labels']['name'], "up")
+# @app.route('/alerts', methods=['POST'])
+# def alerts():
+#     """Receive alerts from prometheus and trigger scale and notify for telegram
 
-    # Send alert to telegram
-    send_test_message(data)
-    return 'OK'
+#     Returns:
+#         string: "OK" for comfirm everything is work
+#     """
+#     data = request.json
+#     # Your analysis and processing here!
+#     for index in range(0, len(data["alerts"])):
+#         if data['alerts'][index]['labels']['name'] not in replica_name:
+#             replica_name.append(data['alerts'][index]['labels']['name'])
+#             threading.Thread(target=scaling_replica, args=(data['alerts'][index]['labels']['name'], 1)).start()
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000")
+#     # Send alert to telegram
+#     # send_test_message(data)
+#     return 'OK'
+
+# Run a webhook for listening the alert and trigger scaling
+# app.run(host="0.0.0.0", port="5000")
+
+# Start thread for checking health of prometheus 
+threading.Thread(target=check_health_alert).start()
+
+while True: 
+    try:
+        print("Result Replica: ", replica_name)
+        print("Result Alert Pull: ", monitoring_alert_prometheus)
+        print("Result ReplicaNumber:", current_replica)
+        print(scale_down_flag)
+        time.sleep(3)
+        response = requests.get('http://localhost:9090/api/v1/alerts')
+        alerts = response.json()['data']['alerts']
+        for index in range(0, len(alerts)):
+            if alerts[index]['labels']['name'] not in replica_name:
+                replica_name.append(alerts[index]['labels']['name'])
+                threading.Thread(target=scaling_replica, args=(alerts[index]['labels']['name'], 1)).start()
+    except KeyboardInterrupt:
+        exit(1)
--- Script/webhook/scale-service-swarm.sh
+++ Script/webhook/scale-service-swarm.sh
@@ -6,18 +6,17 @@ name=$1
 service_name=$(echo "$name" | cut -d "." -f 1)
 
 ## Current number replication is running
-replica_exist=$(docker service ls | grep "$service_name" | awk '{print $4}' | cut -d "/" -f 1)
+replica_really_exist=$(docker service ls | grep "$service_name" | awk '{print $4}' | cut -d "/" -f 2)
 
 ## Scale up or scale down the service
 if [[ $2 == "up" ]]; then
-    docker service scale "$service_name"=$(("$replica_exist" + 1))
+    docker service scale "$service_name"=$(("$replica_really_exist" + $3)) > /dev/null
     exit 0
 else
-    if [[ $(("$replica_exist")) -gt 1 ]]; then
-        docker service scale "$service_name"=$(("$replica_exist" - 1))
+    if [[ $(("$replica_really_exist")) -gt 1 ]]; then
+        docker service scale "$service_name"=$(("$replica_really_exist" - $3)) > /dev/null
         exit 0
     else
-        echo "$service_name is already the lastest, can not scale down the service"
         exit 1
     fi
 fi
